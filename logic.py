d46ba8aec0073fbbf162f77ace1b5d35a0291759
Regina2911 <leamshiiiift@gmail.com>
2025-12-26 11:58:43 +0100

    modified:   logic.py

--- logic.py
+++ logic.py
@@ -1,5 +1,6 @@
 from random import randint
 import requests
+from datetime import datetime, timedelta
 
 class Pokemon:
     pokemons = {}
@@ -12,25 +13,13 @@ class Pokemon:
         self.img = self.get_img()
         self.name = self.get_name()
 
-        self.hp = self.get_hp()
-        self.attack = self.get_attack()
+        self.hp = randint(100, 200)
+        self.power = randint(20, 50)
         self.defense = self.get_defense()
         self.type = self.get_type()
-        
+        self.last_feed_time = datetime.now()
 
         Pokemon.pokemons[pokemon_trainer] = self
-
-
-    def heal(self):
-            self.hp += 10
-
-    def power_up(self):
-        self.attack += 5
-
-    def level_up(self):
-        self.hp += 5
-        self.attack += 3
-        self.defense += 2
 
     # Метод для получения картинки покемона через API
     def get_img(self):
@@ -51,22 +40,6 @@ class Pokemon:
             return (data['forms'][0]['name'])
         else:
             return "Pikachu"
-
-
-    def get_hp(self):
-        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
-        response = requests.get(url)
-        if response.status_code == 200:
-            return response.json()['stats'][0]['base_stat']
-        return 50
-
-
-    def get_attack(self):
-        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
-        response = requests.get(url)
-        if response.status_code == 200:
-            return response.json()['stats'][1]['base_stat']
-        return 50
 
 
     def get_defense(self):
@@ -90,7 +63,7 @@ class Pokemon:
              f"🐾 Имя покемона: {self.name}\n"
         f"🔥 Тип покемона: {self.type}\n"
         f"❤️ Здоровье (HP): {self.hp}\n"
-        f"⚔️ Сила атаки: {self.attack}\n"
+        f"⚔️ Сила атаки: {self.power}\n"
         f"🛡️ Защита: {self.defense}"
         )
 
@@ -98,7 +71,37 @@ class Pokemon:
         return self.img
 
 
+    def attack(self, enemy):
+        if isinstance(enemy, Wizzard): # Проверка на то, что enemy является типом данных Wizard (является экземпляром класса Волшебник)
+            chance = randint(1,5)
+            if chance == 1:
+                return "Покемон-волшебник применил щит в сражении"
+        if enemy.hp > self.power:
+            enemy.hp -= self.power
+            return f"Сражение @{self.pokemon_trainer} с @{enemy.pokemon_trainer}"
+        else:
+            enemy.hp = 0
+            return f"Победа @{self.pokemon_trainer} над @{enemy.pokemon_trainer}! "
+    
 
-   
+    def feed(self, feed_interval = 20, hp_increase = 10 ):
+        current_time = datetime.now()  
+        delta_time = timedelta(seconds=feed_interval)  
+        if (current_time - self.last_feed_time) > delta_time:
+            self.hp += hp_increase
+            self.last_feed_time = current_time
+            return f"Здоровье покемона увеличено. Текущее здоровье: {self.hp}"
+        else:
+            return f"Следующее время кормления покемона: {current_time-delta_time}"
 
 
+class Wizzard(Pokemon):
+    pass
+
+class Fighter(Pokemon):
+    def attack(self, enemy):
+        super_power = randint(10,20)
+        self.power += super_power
+        result = super().attack(enemy)
+        self.power -= super_power
+        return result + f"\nБоец применил супер-атаку силой:{super_power} "
